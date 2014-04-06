fffb0b640e5748cfdbbdb4ef6c95bd715ea8cd19
Greg Wang <gregwym@gmail.com>
2014-04-06 14:43:38 -0400

    Add usr folder and posts API.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.configure(function() {
   app.use(app.router);
   app.use('/src', express.static(__dirname + '/src'));
   app.use('/lib', express.static(__dirname + '/bower_components'));
+  app.use('/usr', express.static(__dirname + '/usr'));
 });
 
 app.get('/', function(req, res) {
@@ -17,6 +18,24 @@ app.get('/', function(req, res) {
 
 app.get('/templates/:name', function(req, res) {
   res.render(req.params.name, {});
+});
+
+app.get('/posts', function(req, res) {
+  var postFileNameRegex = /^([^\.]+)\.(\d+).([\w\d]+)$/;
+  var posts = require('fs').readdirSync(__dirname + '/usr/posts').map(function(filename) {
+    var match = postFileNameRegex.exec(filename);
+    if (!match) { return null; }
+    return {
+      'url': '/usr/posts/' + filename,
+      'title': match[1].replace(/[_-]/g, ' ').toLowerCase(),
+      'date': match[2],
+      'format': match[3]
+    };
+  }).filter(function(entry) {
+    return entry !== null;
+  });
+
+  return res.json(posts);
 });
 
 app.listen(3000, function() {
